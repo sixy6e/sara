64187634fecbb8c96a43b7d31621390d93e87e5c
Chris Allen <chris.allen@nci.org.au>
2017-12-19 11:16:09 +1100

    Split "04_deploy_server.sh" into two scripts

--- 04b_register_collections.sh
+++ 04b_register_collections.sh
@@ -14,8 +14,6 @@ set -o pipefail
 
 CONFIG=
 FORCE=NO
-WWW_USER=nginx:nginx
-PWD=`pwd`
 SRC_DIR=`pwd`
 function showUsage {
     echo ""
@@ -23,8 +21,8 @@ function showUsage {
     echo ""
     echo "   Usage $0 [options]"
     echo ""
-    echo "      -C | --config : local config file containing parameters to build config.php file"
-    echo "      -F | --force : force suppression of endpoint directory (i.e. ${SARA_SERVER_TARGET_DIR}/${SARA_SERVER_VERSION_ENDPOINT} and ${SARA_CLIENT_TARGET_DIR})"
+    echo "      -C | --config : local config file"
+    echo "      -F | --force : overwrite existing collections configuration"
     echo "      -h | --help : show this help"
     echo ""
     echo ""
@@ -67,34 +65,6 @@ fi
 
 # Source config file
 . ${CONFIG}
-
-# Set endpoints
-SARA_SERVER_ENDPOINT=${SARA_SERVER_TARGET_DIR}${SARA_SERVER_VERSION_ENDPOINT}
-
-if [ "${FORCE}" == "YES" ]
-then
-  echo " ==> Suppress ${SARA_SERVER_ENDPOINT}"
-  rm -Rf ${SARA_SERVER_ENDPOINT}
-fi
-
-mkdir -p ${SARA_SERVER_TARGET_DIR}
-
-echo " ==> Deploy resto in ${SARA_SERVER_ENDPOINT}"
-${SRC_DIR}/resto/_install/deploy.sh -s ${SRC_DIR}/resto -t ${SARA_SERVER_ENDPOINT}
-
-# Fix RewriteBase path
-ESCAPED_SARA_PATH=$(echo "${SARA_SERVER_SUB}${SARA_SERVER_VERSION_ENDPOINT}" | sed -r -e 's/\//\\\//g')
-sed -i -r -e "s/\/resto\//${ESCAPED_SARA_PATH}\//g" ${SARA_SERVER_ENDPOINT}/.htaccess
-
-echo " ==> Copy models under ${SARA_SERVER_ENDPOINT}/include/resto/Models"
-cp -R ${SRC_DIR}/sara.server/Models/*.php ${SARA_SERVER_ENDPOINT}/include/resto/Models/
-
-echo " ==> Use ${CONFIG} file to generate ${SARA_SERVER_ENDPOINT}/include/config.php";
-${SRC_DIR}/sara.server/generate_config.sh -C ${CONFIG} > ${SARA_SERVER_ENDPOINT}/include/config.php
-chmod 0600 ${SARA_SERVER_ENDPOINT}/include/config.php
-
-echo " ==> Set ${SARA_SERVER_ENDPOINT} rights to ${WWW_USER}"
-chown -R ${WWW_USER} ${SARA_SERVER_ENDPOINT}
 
 echo " ==> Create property mappings for collections"
 
